5784ada6f1f9f6e0fc0ea932f6da24434e190c75
Matthew DEVILLE <matthew@stockly.ai>
2025-08-22 16:36:36 +0200

    add tests

---
+++ tests/source/configs/reorder_imports/visibility.rs
@@ -0,0 +1,7 @@
+// rustfmt-reorder_imports: Visibility
+// Reorder imports
+
+use ipsum;
+pub use dolor;
+use sit;
+pub(crate) use lorem;
---
+++ tests/source/configs/reorder_modules/visibility.rs
@@ -0,0 +1,8 @@
+// rustfmt-style_edition: 2024
+// rustfmt-reorder_modules: Visibility
+// Reorder modules
+
+pub mod sit;
+mod ipsum;
+mod dolor;
+pub(crate) mod lorem;
